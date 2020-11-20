2bc4ca41c97723925e3f8d6a05f4e85f3e4068fd
Roman Tonkikh <romantonkikh@gmail.com>
2020-11-20 10:55:15 +0300

    Реализовал методы save, get, delete, clear, getAll, size в классе ArrayStorage

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -5,26 +5,98 @@ public class ArrayStorage {
     Resume[] storage = new Resume[10000];
 
     void clear() {
+        int count = 0;
+        for (Resume r : storage) {
+            if (r != null) {
+                storage[count] = null;
+                count++;
+            } else {
+                break;
+            }
+        }
     }
 
-    void save(Resume r) {
+    void save(Resume resume) {
+        int count = 0;
+
+        for (Resume r : storage) {
+            if (r != null) {
+                if (r.uuid.equals(resume.uuid)) {
+                    System.out.println("------Резюме с таким uuid уже есть в базе данных!-----");
+                    break;
+                }
+            }
+            if (r == null) {
+                storage[count] = resume; // здесь нужно доработать, чтобы нельзя было сохранить не уникальное значение
+                break;
+            }
+            count++;
+        }
     }
 
     Resume get(String uuid) {
-        return null;
+        Resume getResume = null;
+        for (Resume r : storage) {
+            if (r == null) {
+                break;
+            } else if (r.uuid.equals(uuid)) {
+                getResume = r;
+                break;
+            }
+        }
+        return getResume;
     }
 
     void delete(String uuid) {
+        int count = 0;
+        for (Resume r : storage) {
+            if (r == null) {
+                break;
+            } else if (r.uuid.equals(uuid)) {
+                storage[count] = null;
+
+            }
+            count++;
+        }
+
+        for (int i = 0; i < count - 1; i++) {
+            if (storage[i] == null) {
+                storage[i] = storage[i + 1];
+                storage[i + 1] = null;
+
+            }
+        }
+
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        int count = 0;
+
+        for (Resume r : storage) {
+            if (r != null) {
+                count++;
+            } else {
+                break;
+            }
+        }
+        Resume[] allResume = new Resume[count];
+        System.arraycopy(storage, 0, allResume, 0, count);
+        return allResume;
     }
 
     int size() {
-        return 0;
+        int count = 0;
+
+        for (Resume r : storage) {
+            if (r != null) {
+                count++;
+            } else {
+                break;
+            }
+        }
+        return count;
     }
 }
