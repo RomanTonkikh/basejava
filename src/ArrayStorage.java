3dc817cac42774c83b6a93ce6ceefa88b5b2ad2e
Roman Tonkikh <romantonkikh@gmail.com>
2020-11-23 13:48:03 +0300

    Убрал проверки на null в методах, так как они не нужны.

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -6,44 +6,35 @@ public class ArrayStorage {
     public static int size = 0;
 
     void clear() {
-        int count = 0;
-        size = 0;
-        for (Resume r : storage) {
-            if (r != null) {
-                storage[count] = null;
-                count++;
-            } else {
-                break;
-            }
+        for (int i = 0; i < size; i++) {
+            storage[i] = null;
         }
+        size = 0;
     }
 
     void save(Resume resume) {
-        int count = 0;
-
-        for (Resume r : storage) {
-            if (r != null) {
-                if (r.uuid.equals(resume.uuid)) {
+        if (size == 0) {
+            storage[0] = resume;
+            size++;
+        } else {
+            for (int i = 0; i < size; i++) {
+                if (storage[i].uuid.equals(resume.uuid)) {
                     System.out.println("------Резюме с таким uuid уже есть в базе данных!-----");
+                    break;
+                } else if (i == size - 1) {
+                    storage[size] = resume;
+                    size++;
                     break;
                 }
             }
-            if (r == null) {
-                storage[count] = resume;
-                size++;
-                break;
-            }
-            count++;
         }
     }
 
     Resume get(String uuid) {
         Resume getResume = null;
-        for (Resume r : storage) {
-            if (r == null) {
-                break;
-            } else if (r.uuid.equals(uuid)) {
-                getResume = r;
+        for (int i = 0; i < size; i++) {
+            if (storage[i].uuid.equals(uuid)) {
+                getResume = storage[i];
                 break;
             }
         }
@@ -51,57 +42,31 @@ public class ArrayStorage {
     }
 
     void delete(String uuid) {
-        int count = 0;
-        for (Resume r : storage) {
-            if (r == null) {
-                break;
-            } else if (r.uuid.equals(uuid)) {
-                storage[count] = null;
+        for (int i = 0; i < size; i++) {
+            if (storage[i].uuid.equals(uuid)) {
+                storage[i] = null;
                 size--;
 
             }
-            count++;
         }
-
-        for (int i = 0; i < count - 1; i++) {
+        for (int i = 0; i < size; i++) {
             if (storage[i] == null) {
                 storage[i] = storage[i + 1];
                 storage[i + 1] = null;
-
             }
         }
-
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        int count = 0;
-
-        for (Resume r : storage) {
-            if (r != null) {
-                count++;
-            } else {
-                break;
-            }
-        }
-        Resume[] allResume = new Resume[count];
-        System.arraycopy(storage, 0, allResume, 0, count);
+        Resume[] allResume = new Resume[size];
+        System.arraycopy(storage, 0, allResume, 0, size);
         return allResume;
     }
 
     int size() {
-        /*int count = 0;
-
-        for (Resume r : storage) {
-            if (r != null) {
-                count++;
-            } else {
-                break;
-            }
-        }
-        return count;*/
         return size;
     }
 }
