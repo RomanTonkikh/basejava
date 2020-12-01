e337af70adbcb1cf831a957691c4ec87aea5eac6
Roman Tonkikh <romantonkikh@gmail.com>
2020-12-01 17:34:57 +0300

    удалил пробелы

--- src/com/urise/webapp/storage/AbstractArrayStorage.java
+++ src/com/urise/webapp/storage/AbstractArrayStorage.java
@@ -7,11 +7,8 @@ import com.urise.webapp.model.Resume;
  */
 public abstract class AbstractArrayStorage implements Storage {
     protected static final int STORAGE_LIMIT = 100_000;
-
     protected Resume[] storage = new Resume[STORAGE_LIMIT];
     protected int size = 0;
-
-
 
     public int size() {
         return size;
