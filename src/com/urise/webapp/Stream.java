68baf3048ed62793b24377e4072659a8c4fa9db5
Roman Tonkikh <romantonkikh@gmail.com>
2021-02-16 12:13:38 +0300

    fixed class Stream

--- src/com/urise/webapp/Stream.java
+++ src/com/urise/webapp/Stream.java
@@ -3,25 +3,22 @@ package com.urise.webapp;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
 public class Stream {
     public static void main(String[] args) {
-        int[] values = {1, 2, 3, 4, 3, 3, 9};
-        List<Integer> integers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9);
+        int[] values = {1, 2, 3, 5, 9, 7, 5, 3};
+        List<Integer> integers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 7, 9);
         System.out.println(minValue(values));
         System.out.println(oddOrEven(integers));
     }
 
     private static int minValue(int[] values) {
-        int[] newValues = Arrays.stream(values).sorted().distinct().toArray();
-        return IntStream.range(0, newValues.length)
-                .map(i -> (int) (Math.pow(10, newValues.length - 1 - i) * newValues[i])).sum();
+        return Arrays.stream(values).distinct().sorted().reduce(0, (x, y) -> 10 * x + y);
     }
 
     private static List<Integer> oddOrEven(List<Integer> integers) {
-        return integers.stream().filter(integers.stream().mapToInt(Integer::intValue)
-                .sum() % 2 == 0 ? s -> s % 2 != 0 : s -> s % 2 == 0)
+        int sum = integers.stream().reduce(0, Integer::sum);
+        return integers.stream().filter(sum % 2 == 0 ? s -> s % 2 != 0 : s -> s % 2 == 0)
                 .collect(Collectors.toList());
     }
 }
